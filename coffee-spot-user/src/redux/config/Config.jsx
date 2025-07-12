0a611422f147d2f365cf3238f2055cf3aeb75559
zain100000 <muhammadzainulabideen292@gmail.com>
2025-07-12 15:17:16 +0500

    Changed the backend url in both Super Admin Panel and App

--- coffee-spot-user/src/redux/config/Config.jsx
+++ coffee-spot-user/src/redux/config/Config.jsx
@@ -1,6 +1,6 @@
 const CONFIG = {
-  BASE_URL: 'http://10.0.2.2:8000/api',
-  // BASE_URL: 'http://192.168.1.7:8000/api',
+  // BASE_URL: 'http://10.0.2.2:8000/api',
+  BASE_URL: 'https://coffee-spot-backend.onrender.com/api',
 };
 
 export default CONFIG;
--- coffee-spot-user/src/utils/customSocket/Config/Config.jsx
+++ coffee-spot-user/src/utils/customSocket/Config/Config.jsx
@@ -1,5 +1,6 @@
 const CONFIG = {
-    BASE_URL: 'http://10.0.2.2:8000',    
+    // BASE_URL: 'http://10.0.2.2:8000',    
+    BASE_URL: 'https://coffee-spot-backend.onrender.com',    
   };
   
   export default CONFIG;
